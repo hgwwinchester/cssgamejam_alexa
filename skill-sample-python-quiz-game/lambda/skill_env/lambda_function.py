e767e4b0d88edc206b9901f8f4464655eb5a7a0a
DavidManda <davidmanda1998@gmail.com>
2018-11-19 17:54:02 +0000

    Added logs

--- skill-sample-python-quiz-game/lambda/skill_env/lambda_function.py
+++ skill-sample-python-quiz-game/lambda/skill_env/lambda_function.py
@@ -23,15 +23,17 @@ class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
+        logger.info("In launch can handle request!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         return is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
         speech_text = "Welcome to the Alexa Skills Kit, you can say hello!"
-
-        handler_input.response_builder.speak(speech_text).set_card(
-            SimpleCard("Hello World", speech_text)).set_should_end_session(
-            False)
+        logger.info("In launch handle request!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        handler_input.response_builder.speak(speech_text)
+        # .set_card(
+        #     SimpleCard("Hello World", speech_text)).set_should_end_session(
+        #     False)
         return handler_input.response_builder.response
 
 
